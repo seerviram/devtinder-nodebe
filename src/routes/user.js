80ee15f4fde35828f7966a6ffd4b125d4211c412
bholaram <bseervi@microsoft.com>
2024-11-03 17:08:59 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuthHandler } = require("../middlwares/utils");
 const connnectionRequestModel = require("../models/connectionRequest");
+const UserModel = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -51,6 +52,44 @@ userRouter.get("/user/requests/connections", userAuthHandler, async (req, res)=>
     catch(err){
         return res.send("invalid request");
     }
+    })
+
+    // feed api
+
+userRouter.get("/feed", userAuthHandler, async(req,res, next)=> {
+    try{
+
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)*limit
+
+        const myConnections = await connnectionRequestModel.find({
+            $or: [{
+                fromUserId: loggedInUser._id
+            }, {
+                toUserId: loggedInUser._id
+            }]
+        });
+        const hideUsersFromFeed = new Set();
+        myConnections.forEach(user=> {
+            hideUsersFromFeed.add(user.fromUserId);
+            hideUsersFromFeed.add(user.toUserId);
+        });
+        // $ne = not equal
+        // $nin = not in
+        const users = await UserModel.find({
+            $and: [ 
+                {_id: { $ne:  loggedInUser._id}},
+                {_id: { $nin: Array.from(hideUsersFromFeed)}}
+            ]
+        }).select("firstName").skip(skip).limit(limit)
+        return res.send(users);
+    }catch(err){
+        return res.status(400).json({
+            message: err.message
+        })
+        }
     })
 
 
